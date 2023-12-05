2bfa637b5de0962a58cae331e7b2269332f9d237
Vi <71643953+vi-kawaii@users.noreply.github.com>
2023-12-05 12:20:55 +0000

    поправил верстку

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
   return (
     <>
       <div className={styles["home"]}>
-        <h1 className={styles["title-page"]}>Конфигуратор ПК</h1>
-        <div className={styles["main"]}>
-          <ConfiguratorTable />
+        <div className="max-w-6xl mx-auto">
+          <h1 className={styles["title-page"]}>Конфигуратор ПК</h1>
+          <div className={styles["main"]}>
+            <ConfiguratorTable />
+          </div>
         </div>
       </div>
     </>
